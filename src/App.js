f0ed5168cce3d7a046b4453b96d88ab1be6f7d7f
unknown <adarsh.gabani@techmayntra.in>
2022-06-13 13:26:44 +0530

    news app started

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import "./App.css";
-import About from "./components/About";
-import Navbar from "./components/Navbar";
-import { useState } from "react";
-import TextForm from "./components/TextForm";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-function App(props) {
-  const [Mode, setMode] = useState("light");
-  const [bgColor, setbgColor] = useState("#ffffff");
 
-  const toggleMode = () => {
-    if (Mode === "dark") {
-      setMode("light");
-    } else {
-      setMode("dark");
-    }
-  };
-  return (
-    <>
-      <Router>
-        <Navbar
-          title="TextUtils"
-          mode={Mode}
-          toggleMode={toggleMode}
-          setBgColor={setbgColor}
-        />
+import React, { Component } from 'react'
+import Navbar from "./components/NavBar";
+import News from "./components/News";
 
-        <div
-          className="container  my-3 "
-          style={{
-            backgroundColor: bgColor,
-          }}
-        >
-          <Routes>
-            <Route path="/about" element={<About />}></Route>
-            <Route
-              path="/"
-              element={<TextForm heading="Enter The text to analyze" />}
-            ></Route>
-          </Routes>
-        </div>
-      </Router>
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div><Navbar/>
+      <News/>
+      
+      </div>
+
+    )
+  }
 }
-
-export default App;
+  
